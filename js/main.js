4e2ecd6367fa3c2658b2346f3b5e98352773d7e2
Marshall Creger <marshallcreger@Marshalls-MacBook-Pro.local>
2017-06-21 16:25:19 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,49 +1,61 @@
 
 var cards = [
 	{
-		"rank": "queen",
-		"suit": "hearts",
-		"cardImage": "images/queen-of-hearts.png"
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: "images/queen-of-hearts.png"
 	},
 	{
-		"rank": "queen",
-		"suit": "diamonds",
-		"cardImage": "images/queen-of-diamonds.png"
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: "images/queen-of-diamonds.png"
 	},
 	{
-		"rank": "king",
-		"suit": "hearts",
-		"cardImage": "images/king-of-hearts.png"
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: "images/king-of-hearts.png"
 	},
 	{
-		"rank": "king",
-		"suit": "diamonds",
-		"cardImage": "images/king-of-diamonds.png"
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: "images/king-of-diamonds.png"
 	}
 ];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-	if (cardsInPlay.length === 2) {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match")
-		} else {
-			alert("Sorry try again")
-		}
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match")
+	} else {
+		alert("Sorry try again")
 	}
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage)
 	console.log("User flipped " + cards[cardId].rank)
 	console.log("The card image is " + cards[cardId].cardImage)
 	console.log("The card suit is " + cards[cardId].suit)
 	cardsInPlay.push(cards[cardId].rank)
-	checkForMatch();
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+	
 
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
 
 
 
